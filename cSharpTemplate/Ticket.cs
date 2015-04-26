d272b6e1719745f7c0f345a5af304bcc651eca7a
unknown <Anton_Grishankov@EPRURYAW0401.moscow.epam.com>
2015-04-26 12:58:04 +0300

    agrishankov: Selling of tickets

---
+++ cSharpTemplate/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpTemplate
+{
+    public class Ticket
+    {
+        /// <summary>
+        /// Category of ticket 
+        /// </summary>
+        public TicketCategory Category;
+
+        /// <summary>
+        /// Place of ticket
+        /// </summary>
+        public int TicketPlace {get; set;}
+
+
+
+    }
+}
