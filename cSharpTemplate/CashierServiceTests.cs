f7fa2f323eb371ecc0dc95bf63c5dc5194ea319d
chulanov <Vladimir_Chulanov@EPRURYAW0400.moscow.epam.com>
2015-04-26 13:07:24 +0300

    added tests for VIP and table sells

--- cSharpTemplate/CashierServiceTests.cs
+++ cSharpTemplate/CashierServiceTests.cs
@@ -10,7 +10,9 @@ namespace cSharpTemplate
 	[TestFixture]
 	class CashierServiceTests
 	{
-		const int EnteryTickersTotal = 3;
+		const int EnteryTickersTotal = 100;
+		const int VIPTickersTotal = 10;
+		const int TableTickersTotal = 25;
 		ClubEvent _event;
 
 		[SetUp]
@@ -45,18 +47,38 @@ namespace cSharpTemplate
 			Assert.IsFalse(cashier.Sell(_event, PriceCategory.Entery));
 		}
 
-		//[Test]
-		//public void SellTableTicket()
-		//{
-		//	CashierService cashier = new CashierService();
-		//	cashier.Sell(PriceCategory.Entery);
-		//}
+		[Test]
+		public void SellVIPTicket()
+		{
+			CashierService cashier = new CashierService(VIPTickersTotal);
+			Assert.IsTrue(cashier.Sell(_event, PriceCategory.VIP));
+		}
 
-		//[Test]
-		//public void SellVIPTicket()
-		//{
-		//	CashierService cashier = new CashierService();
-		//	cashier.Sell(PriceCategory.Entery);
-		//}
+		[Test]
+		public void SellExtraVIPTicket()
+		{
+			CashierService cashier = new CashierService(VIPTickersTotal);
+			for (int i = 0; i < VIPTickersTotal; i++)
+				cashier.Sell(_event, PriceCategory.Entery);
+
+			Assert.IsFalse(cashier.Sell(_event, PriceCategory.VIP));
+		}
+
+		[Test]
+		public void SellTableTicket()
+		{
+			CashierService cashier = new CashierService(TableTickersTotal);
+			Assert.IsTrue(cashier.Sell(_event, PriceCategory.Table));
+		}
+
+		[Test]
+		public void SellExtraTableTicket()
+		{
+			CashierService cashier = new CashierService(TableTickersTotal);
+			for (int i = 0; i < TableTickersTotal; i++)
+				cashier.Sell(_event, PriceCategory.Entery);
+
+			Assert.IsFalse(cashier.Sell(_event, PriceCategory.Table));
+		}
 	}
 }
