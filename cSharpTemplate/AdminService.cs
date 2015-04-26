3ceca29371848f607752f6bd426e301481809974
chulanov <Vladimir_Chulanov@EPRURYAW0400.moscow.epam.com>
2015-04-26 12:28:17 +0300

    formatted

--- cSharpTemplate/AdminService.cs
+++ cSharpTemplate/AdminService.cs
@@ -7,41 +7,43 @@ namespace cSharpTemplate
 	{
 		private readonly List<ClubEvent> club_events;
 
-		public AdminService ()
+		public AdminService()
 		{
-			club_events = new List<ClubEvent> ();
+			club_events = new List<ClubEvent>();
 		}
 
-		public void AddEvent(ClubEvent club_event) 
+		public void AddEvent(ClubEvent club_event)
 		{
-			if (club_event.Title == null) {
-				throw new Exception ("validation failed: title must not be null");
+			if (club_event.Title == null)
+			{
+				throw new Exception("validation failed: title must not be null");
 			}
 
-            if (club_event.EventDateTime == DateTime.MinValue)
-            {
-                throw new Exception("validation failed: DateTime must not be null");
-            }
+			if (club_event.EventDateTime == DateTime.MinValue)
+			{
+				throw new Exception("validation failed: DateTime must not be null");
+			}
 
-            if (club_event.EventDateTime.Date == DateTime.Now.Date || club_event.EventDateTime > DateTime.Now.AddMonths(6))
-            {
-                throw new Exception("validation failed: Date must not be today");
-            }
+			if (club_event.EventDateTime.Date == DateTime.Now.Date || club_event.EventDateTime > DateTime.Now.AddMonths(6))
+			{
+				throw new Exception("validation failed: Date must not be today");
+			}
 
-            if (club_event.Artists == null || club_event.Artists.Count == 0)
-            {
-                throw new Exception("validation failed: Artists list must not be empty");
-            }
+			if (club_event.Artists == null || club_event.Artists.Count == 0)
+			{
+				throw new Exception("validation failed: Artists list must not be empty");
+			}
 
-            if (club_event.TicketPrices == null || club_event.TicketPrices.Count == 0)
-            {
-                throw new Exception("validation failed: Ticket prices list must not be empty");
-            }
+			if (club_event.TicketPrices == null || club_event.TicketPrices.Count == 0)
+			{
+				throw new Exception("validation failed: Ticket prices list must not be empty");
+			}
 
-			club_events.Add (club_event);
+			club_events.Add(club_event);
 		}
 
-		public List<ClubEvent> GetAllEvents() {
+		public List<ClubEvent> GetAllEvents()
+		{
 			return club_events;
 		}
 	}
