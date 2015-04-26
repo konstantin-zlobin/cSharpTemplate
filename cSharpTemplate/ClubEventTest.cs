98ae62a7b10d85f73cc3eeeb39828b19a04bbbfe
unknown <Elena_Ilyina@EPRURYAW0398.moscow.epam.com>
2015-04-26 13:20:22 +0300

    with test stubs

---
+++ cSharpTemplate/ClubEventTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace cSharpTemplate
+{
+    [TestFixture]
+    public class ClubEventTest
+    {
+        ClubEvent clubEvent;
+
+        [SetUp]
+        public void InitClubEvent()
+        {
+            clubEvent = new ClubEvent();
+            clubEvent.Title = "Megashow 12345";
+            clubEvent.DateAndTime = DateTime.Now.AddDays(2);
+            clubEvent.Artists = new List<string> { "Volodya", "Oleg" };
+            clubEvent.TicketCategoriesToPrices.Add(TicketCategories.VIP, 4000);
+            clubEvent.TicketCategoriesToPrices.Add(TicketCategories.Simple, 2000);
+            clubEvent.TicketCategoriesToPrices.Add(TicketCategories.EnterOnly, 400);
+        }
+
+        [Test]
+        public void TicketsCreationTest()
+        {
+            Assert.IsFalse(clubEvent.Tickets.Any(t => t.IsSold));
+
+            Assert.IsTrue(clubEvent.Tickets.Count(t => t.Category == TicketCategories.EnterOnly) == 100);
+        }
+
+        [Test]
+        public void TicketsSellTest()
+        {
+            Assert.Throws<Exception>(
+                delegate
+                {
+                    clubEvent.SellTicket(136);
+                });
+        }
+    }
+}
